920953462c7d2955f659e1f3c9e15d1bd8564321
Ammar Ahmed <114263245+Ammar2852021@users.noreply.github.com>
2023-06-18 15:26:32 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+let sidebar = document.getElementById('sidebar');
+
+burger.addEventListener('click',function myFunction() {
+    var burger = document.getElementById("burger");
+    if (sidebar.style.display === "block") {
+        sidebar.style.display = "none";
+    } else {
+        sidebar.style.display = "block";
+    }
+  }
+)
+
+

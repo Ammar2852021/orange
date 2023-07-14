8f7af9848e537db4133f59b88d3f5ca9fe265b20
Ammar Ahmed <AmmarAhmed132004@outlook.com>
2023-07-14 06:07:36 +0300

    ED

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
+let sidebar = document.getElementById("sidebar");
 
-let sidebar = document.getElementById('sidebar');
-
-burger.addEventListener('click',function myFunction() {
-    var burger = document.getElementById("burger");
-    if (sidebar.style.display === "block") {
-        sidebar.style.display = "none";
-    } else {
-        sidebar.style.display = "block";
-    }
+burger.addEventListener("click", function myFunction() {
+  var burger = document.getElementById("burger");
+  if (sidebar.style.display === "block") {
+    sidebar.style.display = "none";
+  } else {
+    sidebar.style.display = "block";
   }
-)
-
-
+});
 
 // TOP & BUT
 
-let topicons = document.getElementById('top-icons');
-let buticons = document.getElementById('but-icons');
+let topicons = document.getElementById("top-icons");
+let buticons = document.getElementById("but-icons");
 
+buticons.addEventListener("click", function () {
+  buticons.style.display = "none";
+  topicons.style.display = "block";
+});
+topicons.addEventListener("click", function () {
+  topicons.style.display = "none";
+  buticons.style.display = "block";
+});
 
+let orange = document.getElementById("name");
 
-buticons.addEventListener('click',function()
-{   buticons.style.display='none';
-    topicons.style.display='block';})
-topicons.addEventListener('click',function(){
-topicons.style.display='none';
-buticons.style.display='block';})
+orange.addEventListener("click", function () {
+  console.log(12222);
+});

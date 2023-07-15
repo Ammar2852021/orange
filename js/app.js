c87cdbd3e2a30ce260b617d64fe140030ae4412e
Ammar Ahmed <AmmarAhmed132004@outlook.com>
2023-07-15 17:31:54 +0300

    ED

--- js/app.js
+++ js/app.js
@@ -23,41 +23,42 @@ topicons.addEventListener("click", function () {
   buticons.style.display = "block";
 });
 
-let link_vall = document.getElementById("link_vall");
+// let link_vall = document.getElementById("link_vall");
 
-let ALL = document.getElementById("ALL");
-let video = document.getElementById("video");
+// let ALL = document.getElementById("ALL");
+// let video = document.getElementById("video");
 
-let num = document.getElementById("num");
-num.innerText = video.children.length;
-let div = `
-<div class="message anim" style="--delay: .4s">
-<div class="author-img__wrapper video-author video-p">
-<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check">
-<path d="M20 6L9 17l-5-5"></path>
-</svg>
-<div class="author-img"></div>
-</div>
-<div class="msg-wrapper">
-<div class="msg__name video-p-name"> Thomas Hope</div>
-<div class="msg__content video-p-sub"> recusandae doloremque aperiam alias molestias</div>
-</div>
-</div>
-`;
-let fun = 8;
+// let num = document.getElementById("num");
+// num.innerText = video.children.length;
 
-link_vall.addEventListener("click", (e) => {
-  if (e.target.id !== "link_vall") {
-    console.log(e.target.innerText);
-    // let fun = Math.floor(Math.random() * (10 - 5) + 5);
-    if (e.target.innerText == "MOVIES") {
-      fun = 5;
-    }
-    video.innerHTML = div.repeat(fun);
-    num.innerText = fun;
-  }
+// link_vall.addEventListener("click", (e) => {
+//   if (e.target.id !== "link_vall") {
+//     console.log(e.target.innerText);
+//     // let fun = Math.floor(Math.random() * (10 - 5) + 5);
+//     if (e.target.innerText == "MOVIES") {
+//       fun = 5;
+//     }
+//     video.innerHTML = div.repeat(fun);
+//     num.innerText = fun;
+//   }
 
-  ALL.innerText = e.target.innerText;
-});
+//   ALL.innerText = e.target.innerText;
+// });
 
-console.log(video);
+// console.log(video);
+
+// let NEWS_click = document.getElementById("NEWS_click");
+// let SPORTS_click = document.getElementById("SPORTS_click");
+// let MOVIES_click = document.getElementById("MOVIES_click");
+// let SERIES_click = document.getElementById("SERIES_click");
+// let KIDS_click = document.getElementById("MOVIES_click");
+
+// let NEWS = document.getElementById("NEWS");
+// let SPORTS = document.getElementById("SPORTS");
+// let MOVIES = document.getElementById("MOVIES");
+// let SERIES = document.getElementById("SERIES");
+// let KIDS = document.getElementById("MOVIES");
+
+// NEWS_click.addEventListener("click", function () {
+//   NEWS.style.display = "block";
+// });
